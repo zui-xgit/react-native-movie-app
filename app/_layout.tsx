995fe37fc1f941e299599212c6dd6f32d5f19bc2
benny <benardbenny90@gmail.com>
2025-11-07 05:46:14 +0300

    updates

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,37 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
 import { StatusBar } from "react-native";
 import "../global.css";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    async function doAsyncStuff() {
+      try {
+        // do something async here
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsReady(true);
+      }
+    }
+
+    doAsyncStuff();
+  }, []);
+
+  useEffect(() => {
+    if (isReady) {
+      SplashScreen.hide();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
   return (
     <>
       <StatusBar hidden={true} />
